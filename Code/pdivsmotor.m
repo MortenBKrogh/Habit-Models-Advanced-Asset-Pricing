60b6ac8d42c1a7a013e5368089e9c74aa3575fc5
MortenBKrogh <51132382+MortenBKrogh@users.noreply.github.com>
2019-11-11 19:57:25 +0100

    Major Changes

--- Code/pdivsmotor.m
+++ Code/pdivsmotor.m
@@ -3,10 +3,10 @@ function [inside] = pdivsmotor(w)
 % the fixed point. It has as argument only w ~ N (0, sig_w). Returns VALUE of
 % P / D for each iteration over the current value of s {t} in each iteration.
 % ----------------------------------------------------------------------- %
-global delta g gamma s sg lnpd debug rhow sig_w sig
+global delta g gamma s sg lnpd debug rhow sig_w sig 
 s1=strans(s,w);
 inside = delta * exp(g*(1-gamma))*exp(1/2 * (1-rhow^2) * sig_w^2 ) * ...
-    exp(-gamma * (s1 - s )) .*  (1+exp(interp(s1,sg,lnpd)))' .* ...
+    exp(-gamma * (s1 - s )) .*  (1+exp(interp(s1,sg,lnpd)))' .*      ...
     exp((rhow * sig_w/sig - gamma)*w);
 
 
