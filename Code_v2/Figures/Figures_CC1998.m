3c93d16ed7867933b86662f263ad742c34942f38
RasmusJensen96 <51230994+RasmusJensen96@users.noreply.github.com>
2019-11-12 17:08:01 +0100

    Added Stationary distribution

--- Code_v2/Figures/Figures_CC1998.m
+++ Code_v2/Figures/Figures_CC1998.m
@@ -5,3 +5,7 @@ subplot(2,1,1)
 scatter(lnrtsim*1e2,lndctsim*1e2);title("Monthly Returns vs. consumption growth");
 subplot(2,1,2)
 scatter(alnrtsim_pf*1e2,alndctsim_pf*1e2);title("Annual Returns vs. consumption growth");
+
+%% Stationary Density 
+figure;
+fplot(@q_s, [min(log(S)) s_max]);title('Stationary Distribution of s')
--- Code_v2/Functions/lambda.m
+++ Code_v2/Functions/lambda.m
@@ -1,11 +1,9 @@
 function [y]=lambda(s)
-% Função Lambda
-%
-% Retorna o valor da função sensibilidade
+% function Lambda
 global S_bar s_bar s_max verd
 if verd == 0
     if (s <= s_max)
-        y = (1 / S_bar)*sqrt(max(0 , 1-2*(s-s_bar)))-1;
+        y = (1 / S_bar).*sqrt(max(0 , 1-2.*(s-s_bar)))-1;
     else
         y = 0;
     end;
