0a5f4868f26abccb11f6a2d0d3803cfa485c4ea6
RasmusJensen <201801915@post.au.dk>
2019-11-24 17:59:30 +0100

    .

--- Code_v2/LoadData.m
+++ Code_v2/LoadData.m
@@ -13,27 +13,27 @@ opts.Colors     = get(groot,'defaultAxesColorOrder');
 %%
 PD_Claim_Regressions = 0; % 0 = PC
                           % 1 = PD
+annual = 0;               % 0 = monthly
+                          % 1 = annual
 
-momPC = readtable('PC_Claim_Sim_mom.txt');
-momPD = readtable('PD_Claim_Sim_mom.txt');
-datPC = readtable('PC_Claim_Sim_dat.txt');
-datPD = readtable('PD_Claim_Sim_dat.txt');
-figure;
-subplot(2,1,1)
-plot(table2array(datPC(:,4)));title('$P/C$','Interpreter','latex')
-subplot(2,1,2)
-plot(table2array(datPD(:,4)));title('$P/D$','Interpreter','latex')
+% momPC = readtable('PC_Claim_Sim_mom.txt');
+% momPD = readtable('PD_Claim_Sim_mom.txt');
+% datPC = readtable('PC_Claim_Sim_dat.txt');
+% datPD = readtable('PD_Claim_Sim_dat.txt');
+% figure;
+% subplot(2,1,1)
+% plot(table2array(datPC(:,4)));title('$P/C$','Interpreter','latex')
+% subplot(2,1,2)
+% plot(table2array(datPD(:,4)));title('$P/D$','Interpreter','latex')
 %%
-if PD_Claim_Regressions == 0
-    Moments = momPC;
-    Data = datPC;
-else
-    Moments = momPD;
-    Data = datPD;
-end
+% if PD_Claim_Regressions == 0
+%     Moments = momPC;
+%     Data = datPC;
+% else
+%     Moments = momPD;
+%     Data = datPD;
+% end
 
-aretssim =  table2array(Data(:,4));
-astsim = table2array(Data(:,1));
 if PD_Claim_Regressions == 0
     load('PC_Claim_workspace','s_bar','s_max',...
         'verd','S_bar','sig','gamma','S','astsim_pf','alnrtsim_pf');
@@ -55,6 +55,11 @@ idx_to   = find(NBER_REC.textdata(:,1)==string(to)) - 1;
 % Calculate percentage of the time the economy is in recession
 rec_emp_percentage = sum(NBER_REC.data(idx_from:idx_to,1)) / length(NBER_REC.data(idx_from:idx_to,1));
 rec_emp_percentagetotal = sum(NBER_REC.data(1:end,1)) / length(NBER_REC.data(1:end,1));
+
+labeledMatrix = bwlabel(NBER_REC.data(idx_from:idx_to,1));
+measurements = regionprops(labeledMatrix, 'Area');
+RecessionLengths = [measurements.Area];
+mean(RecessionLengths)/12;
 %%
 s_bar_2 = log(0.02); % Recession specification by figure
 Rec_s_bar = fzero(@(x) (integral(@q_s,-Inf,x) - rec_emp_percentage), s_bar-0.1);
@@ -68,25 +73,30 @@ load('PC_Claim_workspace','astsim_pf');astsim = astsim_pf;
 width = location(2) - location(1);
 heights = heights / (size(astsim, 1) * width);
 %%
-warning('off','all'); % fplot doesnt like the integral functions
-figure;
-barplot = bar(location, heights,'hist');
-barplot.FaceColor = [0, 0.4470, 0.7410];
-hold on
-fplot(@q_s, [min(log(S)-0.5) s_max+0.25],'Color',[0.8500, 0.3250, 0.0980],'LineWidth',2.5);%title('Stationary Distribution of s');
-hold on
-xline(Rec_s_bar,'--','$\bar{s}_{rec}$','Interpreter','latex','FontSize',18);
-hold on
-xline(s_bar,'--','$\bar{s}$','Interpreter','latex','FontSize',18);
-xline(s_bar_2,'--','$\bar{s}_{2,rec}$','Interpreter','latex','FontSize',18);
-xlim([min(log(S))-0.5 -2]);
-legend('Histogram','Theoretical Density','Location','northwest')
-hold off
-saveas(gcf,'../Figures/DistributionS_t','epsc')
+% warning('off','all'); % fplot doesnt like the integral functions
+% figure;
+% barplot = bar(location, heights,'hist');
+% barplot.FaceColor = [0, 0.4470, 0.7410];
+% hold on
+% fplot(@q_s, [min(log(S)-0.5) s_max+0.25],'Color',[0.8500, 0.3250, 0.0980],'LineWidth',2.5);%title('Stationary Distribution of s');
+% hold on
+% xline(Rec_s_bar,'--','$\bar{s}_{rec}$','Interpreter','latex','FontSize',18);
+% hold on
+% xline(s_bar,'--','$\bar{s}$','Interpreter','latex','FontSize',18);
+% xline(s_bar_2,'--','$\bar{s}_{2,rec}$','Interpreter','latex','FontSize',18);
+% xlim([min(log(S))-0.5 -2]);
+% legend('Histogram','Theoretical Density','Location','northwest')
+% hold off
+%saveas(gcf,'../Figures/DistributionS_t','epsc')
 %% Redefining recession periods in the simulation
 % such that the frequency of recession in the simulation corresponds to the
 % empirical frequency of recessions:
 % Recession s_t < Rec_s_bar
+if annual 
+    load('PC_Claim_workspace','astsim_pf');astsim = astsim_pf;
+else
+    load('PC_Claim_workspace','stsim');astsim = stsim;
+end
 rec_sim_ss = NaN(length(astsim), 1);
 for i = 1:length(astsim)
     if astsim(i) < Rec_s_bar
@@ -97,11 +107,28 @@ for i = 1:length(astsim)
 end
 rec_sim_ss_percentage = sum(rec_sim_ss(:)==1) / length(rec_sim_ss);
 %%
-load('PD_Claim_workspace','s_bar','s_max',...
+labeledMatrix = bwlabel(rec_sim_ss);
+measurements = regionprops(labeledMatrix, 'Area');
+RecessionLengths = [measurements.Area];
+mean(RecessionLengths)
+%%
+if annual
+    load('PD_Claim_workspace','s_bar','s_max',...
         'verd','S_bar','sig','gamma','S','astsim','alnrtsim_pf','alnpctsim_pf','Erfinterp_pf');
-PD_regress = alnpctsim_pf;              % PD
-load('PC_Claim_workspace','alnrtsim_pf','alnpctsim_pf','Erfinterp_pf');
-PC_regress = alnpctsim_pf;              % PC
+    PD_regress = alnpctsim_pf;              % PD
+    load('PC_Claim_workspace','alnrtsim_pf','alnpctsim_pf');
+    PC_regress = alnpctsim_pf;              % PC
+else
+    load('PD_Claim_workspace','s_bar','s_max',...
+        'verd','S_bar','sig','gamma','S','stsim','lnrtsim','lnpctsim','Erfinterp_pf');
+    Erfinterp_pf = Erfinterp_pf./12;
+    PD_regress = lnpctsim(2:end,1);              % PD
+    load('PC_Claim_workspace','lnrtsim','lnpctsim')
+    alnrtsim_pf = lnrtsim;
+    PC_regress = lnpctsim(2:end,1);              % PC
+    rec_sim_ss = rec_sim_ss(2:end,1);
+end
+%%
 rfr  = Erfinterp_pf;                    % Risk free rate
 rets = alnrtsim_pf - rfr;               % Excess Returns
 h    =  1;                              % Forecast Horizon 0 = in-sample regression
@@ -241,8 +268,15 @@ x = astsim_pf(1:end-1,:);
 x1 = astsim_pf(2:end,:);
 autocorrX = x\x1;
 %%
+if annual
 load('PD_Claim_workspace','alnpctsim_pf'); PDratio = alnpctsim_pf;
 load('PC_Claim_workspace','alnpctsim_pf'); PCratio = alnpctsim_pf;
+name = '../Figures/PCPD_chain';
+else
+load('PD_Claim_workspace','lnpctsim'); PDratio = lnpctsim;
+load('PC_Claim_workspace','lnpctsim'); PCratio = lnpctsim;
+name = '../Figures/PCPDMonthly_chain';
+end
 subplot(2,1,1)
 plot(PCratio);ylabel('$p_t-c_t$','FontSize',14,'Interpreter','latex');
 title({'$P/C$', ['$E(p_t-c_t)$ =',num2str(mean(PCratio),4)]},'Interpreter','latex');
@@ -250,7 +284,7 @@ subplot(2,1,2)
 plot(PDratio);ylabel('$p_t-d_t$','FontSize',14,'Interpreter','latex');
 title({'$P/D$', ['$E(p_t-d_t)$ =',num2str(mean(PDratio),4)]},'Interpreter','latex');
 ylim([1.25 3.5]);
-saveas(gcf,'../Figures/PCPD_chain','epsc');
+saveas(gcf,name,'epsc');
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%    Long run regressions based on simulated data     %%%
@@ -333,3 +367,21 @@ CC_PD_Stder = Stdexrettinterp_pf;
 CC_PD_Epd = Ep_d_pf;
 CC_PD_StdPD = Stdp_d_pf;
 Simulatedmom;
+
+%%
+clearvars -except Rec_s_bar
+load('PC_Claim_workspace','stsim')
+rec_sim_ss = NaN(length(stsim), 1);
+for i = 1:length(stsim)
+    if stsim(i) < Rec_s_bar
+        rec_sim_ss(i) = 1;
+    else
+        rec_sim_ss(i) = 0;
+    end
+end
+rec_sim_ss_percentage = sum(rec_sim_ss(:)==1) / length(rec_sim_ss);
+%%
+labeledMatrix = bwlabel(rec_sim_ss);
+measurements = regionprops(labeledMatrix, 'Area');
+RecessionLengths = [measurements.Area];
+mean(RecessionLengths)/12;
