da77c3464d3081769f37aca6761cad00c6322e94
RasmusJensen <201801915@post.au.dk>
2019-12-04 12:01:50 +0100

    Updating

--- Code_v2/EmpiricalRegressions.m
+++ Code_v2/EmpiricalRegressions.m
@@ -12,16 +12,33 @@ rx  = datfile.data(:,3); % Returns less dividinds
 dp =  (1+r)./(1+rx) - 1; % Div. Yield
 dd = [NaN ; dp(2:end)./dp(1:end-1).*(1+rx(2:end))-1];  % Div. Growth
 %%
-RR = importdata('Bonds_INF.csv');
+RR = importdata('Rets30dayTBILL.csv');
 Bond = RR.data(:,2); % Returns on 90 day T-bill
-Infl = RR.data(:,4); % Inflation rate measured as CPI
+Infl = RR.data(:,3); % Inflation rate measured as CPI
 RFR = Bond - Infl; % Real Risk free rate
 re = r-RFR; % Excess Returns
 %%
-RecData = importdata('USREC.csv');
-Rec = Rec.data(:,1);
+RecData = importdata('USREC_Period.csv');
+Rec = RecData.data(:,1);
 %%
 T = length(dp);
-rhv = [ones(T-1,1) dp(1:T-1)]; 
-lhv = re(2:end); 
-nwest(lhv,rhv,0)
+dpRec = dp .* Rec;
+reRec = re .* Rec;
+
+a = [dpRec(1:end-1), reRec(2:end)];
+a = a(all(a,2),:);
+
+rhv = [ones(length(a),1), a(:,1)]; 
+lhv = a(:,2);
+Rec_Reg = nwest(lhv,rhv,1)
+%%
+T = length(dp);
+dpExp = dp .* (1-Rec);
+reExp = re .* (1-Rec);
+
+a = [dpExp(1:end-1), reExp(2:end)];
+a = a(all(a,2),:);
+
+rhv = [ones(length(a),1), a(:,1)]; 
+lhv = a(:,2);
+Rec_Reg = nwest(lhv,rhv,1)
